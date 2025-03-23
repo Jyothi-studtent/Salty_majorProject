40a37cceb4c4019c9f4ad6c71cff7a8366925aef
Jyothi-studtent <jy5088010@gmail.com>
2025-03-23 11:31:57 +0000

    v2

--- frontend/src/containers/modal.js
+++ frontend/src/containers/modal.js
@@ -3,9 +3,9 @@ import './css/modal.css';
 
 const Modal = ({ children, onClose, message = null }) => {
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <button className="modal-close" onClick={onClose}>X</button>
+    <div className="modal-overlay-general" onClick={onClose}>
+      <div className="modal-content-general" onClick={(e) => e.stopPropagation()}>
+        <button className="modal-close-general" onClick={onClose}>X</button>
         {/* Render message if provided, otherwise render children */}
         {message ? <p>{message}</p> : children}
       </div>
